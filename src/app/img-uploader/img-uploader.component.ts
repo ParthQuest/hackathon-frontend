4792d531d7b94d26a369234d11e87871af0c4e52
Rajesh Mali <rajesmali8483@gmail.com>
2022-11-19 18:28:40 +0530

    changes

--- src/app/img-uploader/img-uploader.component.ts
+++ src/app/img-uploader/img-uploader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Uploader, UploaderOptions, UploaderResult } from "uploader";
+import { Uploader, UploaderOptions } from "uploader";
+import { ImgUploaderService } from './img-uploader.service';
 
 @Component({
   selector: 'app-img-uploader',
@@ -7,43 +8,51 @@ import { Uploader, UploaderOptions, UploaderResult } from "uploader";
   styleUrls: ['./img-uploader.component.scss']
 })
 export class ImgUploaderComponent implements OnInit {
+  constructor(private imgUploadService: ImgUploaderService) { }
+
+  ngOnInit(): void { }
 
   uploader = new Uploader({
-    apiKey:'public_12a1xsVnXdJvb92LMVcTZkKtDDV1'
+    apiKey: 'public_12a1xsVnXdJvb92LMVcTZkKtDDV1'
     //apiKey:'free'
   });
-  
-  uploadOptions: any = {
+
+  uploadOptions: UploaderOptions = {
     multi: false,
     // Image Editor
     editor: {
       images: {
-        crop: false 
+        crop: false
       }
     },
-   // Accepted Files
+    // Accepted Files
     maxFileCount: 10,
     maxFileSizeBytes: 5 * 1024 * 1024,
-    mimeTypes: ["image/jpg","image/jpeg","image/png"],
-    path: "/uploads" ,
+    mimeTypes: ["image/jpg", "image/jpeg", "image/png"],
     styles: {
-           // Colors & Font Size
-          colors: {
-            primary: "#377dff", 
-            active: "#528fff"
-          },
-          fontSizes: {
-            base: 16
-          }
-        }
+      // Colors & Font Size
+      colors: {
+        primary: "#377dff",
+        active: "#528fff"
+      },
+      fontSizes: {
+        base: 16
+      }
+    },
+
+    onUpdate: (files) => {
+      var cdnUrl = files[0].fileUrl;
+      console.log(cdnUrl);
+      this.getTagList(cdnUrl);
+    },
+
   };
 
-  constructor() { }
-
-  ngOnInit(): void { }
-
-  onUploadComplete(files: UploaderResult[]) {
-    console.log(files);
-  };
-
+  async getTagList(url:string) {
+    debugger
+      this.imgUploadService.getTagList(url).then(tagItems => {
+        var tagList = tagItems;
+        console.log(tagList);
+      })
+  }
 }
--- src/app/img-uploader/img-uploader.service.ts
+++ src/app/img-uploader/img-uploader.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from "@angular/core";
+import { ApiService } from "../api-service/api-service.service";
+import { responseStatus } from "../common";
+const imaggaapiKey = "acc_5c94b16457cdb5b";
+const imaggaapiSecret = "7aa7009ce060d9dda316c81401ed5db9";
+const imaggaURL = 'https://api.imagga.com/v2/tags?image_url=';
 
 @Injectable({
   providedIn: "root"
 })
-export class ImgUploaderService { }
+export class ImgUploaderService { 
+  constructor(private apiService: ApiService) { }
+
+  async getTagList(url:string) {
+    var imaggaUrl = imaggaURL + encodeURIComponent(url);
+    return this.apiService.GETCallAsync<any>(imaggaUrl, {username: imaggaapiKey, password: imaggaapiSecret}).then(response => {
+      if (response.ResponseStatus == responseStatus.Success)
+        return response.ResponseData;
+      else
+        throw new Error(response.ErrorData?.Error ?? response.Message);
+    }).catch(error => {
+      throw error;
+    });
+  }
+}
